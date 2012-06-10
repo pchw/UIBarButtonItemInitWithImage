fd7ad8dec4a9b0f955cd5b84b2a09397d3bc78c4
pochi <pochi@Hina.local>
2012-06-10 17:06:28 +0900

    comment inand add readme.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ UIBarButtonItemInitWithImage
 sample for UIBarButtonItem.alloc.initWithImage not fround
 
 resource/camera.png is from http://glyphish.com/
+
+## Error
+    *** Terminating app due to uncaught exception 'NoMethodError', reason: ' 92:in `application:didFinishLaunchingWithOptions:': undefined method `initWithImage' for #<UIBarButtonItem:0x6c268d0> (NoMethodError)
+    '
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,18 +1,20 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-=begin
+
+    #NG
   	image = UIBarButtonItem.alloc.initWithImage(
   		UIImage.imageNamed "camera.png", 
   		style: UIBarButtonItemStyleBordered, 
   		target: self, 
   		action: "onClicked:")
-=end
 
+=begin
+  # OK
 	btn = UIBarButtonItem.alloc.initWithBarButtonSystemItem(
 		UIBarButtonSystemItemAdd,
 		target:self , 
 		action: "onClicked:")
-
+=end
     true
   end
   def onClicked(sender)
